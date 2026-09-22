0270bf9de81ca6b2af7cda87429450f1e7c1e92b
TiagoDantas17 <tmgd2006@gmail.com>
2026-09-22 17:30:49 +0100

    velocity

---
+++ Assets/Script/MoveShell.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShell : MonoBehaviour
+{
+    float speed = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.transform.transform.Translate(0, Time.deltaTime * speed * 0.5f, Time.deltaTime * speed);
+    }
+}
